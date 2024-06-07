b6b77546c192906637cd305ef36d364c1ac0a406
ajain45 <ajain45@students.cs.ubc.ca>
2024-06-07 12:10:22 +0530

    Made a basic structure of what simple banking application will look like and will update starting Model Class

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -1,4 +1,54 @@
 package Model;
 
 public class Account {
+    // represents a bank account and should have attributes like account number, account holder name, account balance, etc.
+
+    private String accountNumber;
+    private String accountHolderName;
+    private double accountBalance;
+
+    public Account (String accountNumber, String accountHolderName, double accountBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.accountBalance = accountBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public void deposit(double amount) {
+        accountBalance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (accountBalance - amount < 0) {
+            System.out.println("Insufficient balance");
+        } else {
+            accountBalance -= amount;
+        }
+    }
+
+
+
 }
